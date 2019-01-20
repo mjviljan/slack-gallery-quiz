9aaa57fc3d40e33dc1338ab1f78ed3c07f6fa947
Mika Viljanen <mika.viljanen@wunderdog.fi>
2019-01-20 10:37:04 +0200

    Tell the correct answer if user answered wrong (fix earlier commit)

---
+++ src/components/WrongAnswerFeedback/index.tsx
@@ -0,0 +1,23 @@
+import './index.less'
+
+import { QuizUser } from '../../logic/quiz/types'
+import * as React from "react"
+
+export interface AnswerFeedbackProps { user: QuizUser }
+
+export class WrongAnswerFeedback extends React.Component<AnswerFeedbackProps, {}> {
+
+    render() {
+        let correctAnswer = this.props.user.firstName
+        if (this.props.user.firstName.toLowerCase() !== this.props.user.nickname.toLowerCase()) {
+            correctAnswer += ` (${this.props.user.nickname})`
+        }
+        return (
+            <div className="AnswerFeedback">
+                <img src={this.props.user.imageUrl} />
+                <div className="Question">Not quite.</div>
+                <div className="CorrectAnswer">{correctAnswer}</div>
+            </div>
+        )
+    }
+}
