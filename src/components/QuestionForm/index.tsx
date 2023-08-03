1c21370cdf45ee25fc8a6f10f58d55fba105f3ba
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-08-03 11:10:33 +0300

    Convert QuestionForm to a functional component

--- src/components/QuestionForm/index.tsx
+++ src/components/QuestionForm/index.tsx
@@ -2,6 +2,7 @@ import './index.less'
 
 import { QuizUser } from '../../types/types'
 import * as React from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ImageLoader } from '../ImageLoader'
 
 export interface QuestionFormProps {
@@ -11,60 +12,47 @@ export interface QuestionFormProps {
     imageOnloadHandler: () => void
 }
 
-export class QuestionForm extends React.Component<QuestionFormProps, { guess: string }> {
-    _answerField: React.RefObject<HTMLInputElement>
+export const QuestionForm = ({ user, answerHandler, userImageLoaded, imageOnloadHandler }: QuestionFormProps) => {
+    const [guess, setGuess] = useState('')
+    const answerField = useRef<HTMLInputElement>(null)
 
-    constructor(props: QuestionFormProps) {
-        super(props)
-        this.state = { guess: '' }
-        this._answerField = React.createRef()
+    const handleGuessChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setGuess(event.currentTarget.value)
     }
 
-    handleGuessChange = (event: React.FormEvent<HTMLInputElement>) => {
-        this.setState({ guess: event.currentTarget.value })
-    }
-
-    submitAnswer = (event: React.FormEvent<HTMLFormElement>) => {
+    const submitAnswer = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        this.props.answerHandler(this.state.guess)
+        answerHandler(guess)
 
-        this.setState({ guess: '' })
+        setGuess('')
     }
 
-    setFocusToAnswerField = () => {
-        const answerField = this._answerField.current
-        if (answerField) {
-            answerField.focus()
+    const setFocusToAnswerField = () => {
+        const field = answerField.current
+        if (field) {
+            field.focus()
         }
     }
 
-    componentDidMount() {
-        this.setFocusToAnswerField()
-    }
+    useEffect(() => {
+        setFocusToAnswerField()
+    })
 
-    componentDidUpdate() {
-        this.setFocusToAnswerField()
-    }
-
-    render() {
-        if (this.props.userImageLoaded) {
-            return (
-                <div className="QuestionForm">
-                    <img src={this.props.user.imageUrl}/>
-                    <div className="Question">Who is this?</div>
-                    <form onSubmit={this.submitAnswer}>
-                        <input
-                            ref={this._answerField}
-                            type="text"
-                            value={this.state.guess}
-                            onChange={this.handleGuessChange}
-                        />
-                    </form>
-                </div>
-            )
-        } else {
-            return <ImageLoader userImageUrl={this.props.user.imageUrl}
-                                imageOnloadHandler={this.props.imageOnloadHandler}/>
-        }
-    }
+    return userImageLoaded
+        ? (
+            <div className='QuestionForm'>
+                <img src={user.imageUrl} alt='Picture of a doggo' />
+                <div className='Question'>Who is this?</div>
+                <form onSubmit={submitAnswer}>
+                    <input
+                        ref={answerField}
+                        type='text'
+                        value={guess}
+                        onChange={handleGuessChange}
+                    />
+                </form>
+            </div>
+        )
+        : <ImageLoader userImageUrl={user.imageUrl}
+                       imageOnloadHandler={imageOnloadHandler} />
 }
