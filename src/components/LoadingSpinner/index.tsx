23e0ea5f39d2a3dde30fa6a5a9040bfb1adbe1ff
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-08-03 09:14:26 +0300

    Convert LoadingSpinner to a functional component

--- src/components/LoadingSpinner/index.tsx
+++ src/components/LoadingSpinner/index.tsx
@@ -2,19 +2,15 @@ import './index.less'
 
 import * as React from 'react'
 
-export class LoadingSpinner extends React.Component<{}, {}> {
-    render() {
-        return (
-            <div className="LoadingSpinner">
-                <div className="Circle">
-                    <div className="Hub" />
-                    <div className="ShortArm" />
-                    <div className="LongArm" />
-                </div>
-                <div className="Text">
-                    <p>Loading</p>
-                </div>
-            </div>
-        )
-    }
-}
+export const LoadingSpinner = () => (
+    <div className='LoadingSpinner'>
+        <div className='Circle'>
+            <div className='Hub' />
+            <div className='ShortArm' />
+            <div className='LongArm' />
+        </div>
+        <div className='Text'>
+            <p>Loading</p>
+        </div>
+    </div>
+)
