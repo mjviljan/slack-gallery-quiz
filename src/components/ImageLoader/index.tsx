2c9be4dca9d787a1831bf391f4e55fe47f2be0e7
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-08-03 09:18:31 +0300

    Convert ImageLoader to a functional component

--- src/components/ImageLoader/index.tsx
+++ src/components/ImageLoader/index.tsx
@@ -1,27 +1,24 @@
 import './index.less'
 
 import * as React from 'react'
-import { QuizUser } from '../../types/types'
 
 export interface ImageLoaderProps {
     userImageUrl: string
     imageOnloadHandler: () => void
 }
 
-export class ImageLoader extends React.Component<ImageLoaderProps, {}> {
-    render() {
-        return (
-            <div className="ImageLoader">
-                <div className="Circle">
-                    <div className="Hub"/>
-                    <div className="ShortArm"/>
-                    <div className="LongArm"/>
-                </div>
-                <div className="Text">
-                    <p>Loading</p>
-                </div>
-                <img className="ImageBeingLoaded" src={this.props.userImageUrl} onLoad={this.props.imageOnloadHandler}/>
+export const ImageLoader = ({ userImageUrl, imageOnloadHandler }: ImageLoaderProps) =>
+    (
+        <div className='ImageLoader'>
+            <div className='Circle'>
+                <div className='Hub' />
+                <div className='ShortArm' />
+                <div className='LongArm' />
             </div>
-        )
-    }
-}
+            <div className='Text'>
+                <p>Loading</p>
+            </div>
+            <img className='ImageBeingLoaded' src={userImageUrl} onLoad={imageOnloadHandler}
+                 alt="Hidden placeholder for doggo's picture" />
+        </div>
+    )
